b4adeaee33de9bd38c04095021f39c240866f091
Leah <elloworldt@gmail.com>
2021-11-30 07:52:39 -0800

    commit tree 2d13fffac7dc7471d2ccc0419b190f09fbf452a9

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 let currentUser = 'GRACE HOPPER';
-let welcomeMessage = "Welcome to Flatbook, " ${currentUser}!`;
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " ${currentUser}!`;
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
+let shortGreeting = "Welcome, " + currentUser.slice(0, 1) + "!";
